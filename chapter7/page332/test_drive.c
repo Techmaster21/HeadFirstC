b7b6b1a9225ad14633b238eb16b6de59e657fa1e
Adrian Bolt <techmaster211@gmail.com>
2014-08-07 15:49:41 -0500

    Continuing chapter 7

--- chapter7/page332/test_drive.c
+++ chapter7/page332/test_drive.c
@@ -1,3 +1,42 @@
+/* [ADDED] This application has been modified slightly from the original, mainly
+ * to utilize the other functions which the book ignored. 
+ * Also, in case you forgot (I know I did), Ascending is from top to bottom, 
+ * smallest first and descending is top to bottom, largest first. Ascending is 
+ * alphabetical order for characters.
+ * 
+ * Qsort by default sorts in ascending order. If this is confusing, let's think
+ * in terms of the first function. If we return a number that is positive, that 
+ * means that we consider score_a to be greater than score_b and want it to be 
+ * placed after it. If we return a negative number, it means we consider score_a 
+ * less than score_b and it should be placed before it.
+ * 
+ * If this still doesn't make sense, it may help to think of it not in terms of 
+ * less than or greater than but in more simple terms: 
+ * If we return a positive number, we want the first argument to come after the 
+ * second. If we return a negative number, we want the first to come before the 
+ * second. If we return a zero, we don't care what order they're in.
+ * In order to make ascending order into descending order, we simply send the 
+ * opposite of what we sent for ascending - the negative of the original value. 
+ * Or, we can also flip-flop the variables a and b, which accomplishes the same 
+ * end (What's the difference between 9 - 4 and 4 - 9?)
+ */
+/* [ADDED] Though apparently later omitted from the book, probably because they 
+ * are not POSIX defined but rather native to BSD(A Unix based OS like Linux on 
+ * which Mac OSX is based), there are two more sort functions available to you 
+ * if you #include <bsd/stdlib.h>: mergesort() and heapsort(). They are passed 
+ * the same arguments and differ only in their sorting algorithm. heapsort() is
+ * generally considered useless, as it is slower than qsort(). The only 
+ * advantage heapsort has over qsort is that heapsort uses almost no additional 
+ * memory. Both heapsort and qsort are considered unstable. This means that if 
+ * two elements are equal, heapsort and qsort will not necessarily leave them in 
+ * their original order: They may switch their order around in the array. 
+ * mergesort is stable. mergesort can also see faster sorting if the array has 
+ * data that is already in a pre-existing order. mergesort requires the size of 
+ * the objects to be sorted to be greater than sizeof(void *)/2. Generally 
+ * speaking, qsort is the fastest followed by mergesort followed by heapsort.
+ * mergesort() also allocates memory unlike the other two.
+ * My apologies for the wall of text.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,7 +76,7 @@ int compare_names(const void* a, const void* b)
 	return strcmp(*sa, *sb);
 }
 
-int compare_areas_desc(const void* a, const void*b)
+int compare_areas_desc(const void* a, const void* b)
 {
 	return compare_areas(b, a);
 }
