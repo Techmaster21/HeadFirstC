8dba6b876d3fe7f8668fb37508eae7dfc891aadb
Adrian Bolt <techmaster211@gmail.com>
2014-10-26 15:39:10 -0500

    Finished Chapter 9

--- chapter9/page418/newshound.c
+++ chapter9/page418/newshound.c
@@ -5,9 +5,13 @@
 
 int main(int argc, char *argv[])
 {
-	char *feeds[] = {"http://www.cnn.com/rss/celebs.xml",
-	                 "http://www.rollingstone.com/rock.xml",
-	                 "http://eonline.com/gossip.xml"};
+	char *feeds[] = {"file:celebs.xml",
+	                 "file:rock.xml",
+	                 "file:gossip.xml"};
+	                 /* [ADDED] The above was changed from the book so as to
+	                  * actually demonstrate the concept, by using real, 
+	                  * local rss feeds
+	                  */
 	int times = 3;
 	char *phrase = argv[1];
 	int i;
@@ -15,8 +19,13 @@ int main(int argc, char *argv[])
 		char var[255];
 		sprintf(var, "RSS_FEED=%s", feeds[i]);
 		char *vars[] = {var, (char*) NULL};
-		if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py", 
+		if (execle("/usr/bin/python2", "/usr/bin/python2", "./rssgossip.py", 
 		           phrase, (char*)NULL, vars) == -1) {
+			/* [ADDED] Above changed from book so that script actually works,
+			 * as /usr/bin/python defaults to python 3 now, and causes the 
+			 * script to throw out multiple errors. If it doesn't work, try
+			 * changing it back to /usr/bin/python
+			 */
 			fprintf(stderr, "Can't run script: %s\n", strerror(errno));
 			return 1;
 		}
--- chapter9/page424/newshound.c
+++ chapter9/page424/newshound.c
@@ -1,3 +1,7 @@
+/* [ADDED] The output might not look exactly like in the book since all the 
+ * processes run simultaneously (aka asynchronously), they also output at 
+ * different times (which one outputs when varies with each run)
+ */
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -5,9 +9,13 @@
 
 int main(int argc, char *argv[])
 {
-	char *feeds[] = {"http://www.cnn.com/rss/celebs.xml",
-	                 "http://www.rollingstone.com/rock.xml",
-	                 "http://eonline.com/gossip.xml"};
+	char *feeds[] = {"file:celebs.xml",
+	                 "file:rock.xml",
+	                 "file:gossip.xml"};
+	                 /* [ADDED] The above was changed from the book so as to
+	                  * actually demonstrate the concept, by using real, 
+	                  * local rss feeds
+	                  */
 	int times = 3;
 	char *phrase = argv[1];
 	int i;
@@ -15,11 +23,22 @@ int main(int argc, char *argv[])
 		char var[255];
 		sprintf(var, "RSS_FEED=%s", feeds[i]);
 		char *vars[] = {var, (char*) NULL};
-		pid_t pid = fork();
-		if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py", 
-		           phrase, (char*)NULL, vars) == -1) {
-			fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+		pid_t pid = fork(); /* [ADDED] fork() lives in unistd.h by the way */
+		if (pid == -1) {
+			fprintf(stderr, "Can't fork process: %s\n", strerror(errno));
 			return 1;
+		}
+		if (!pid) {
+			if (execle("/usr/bin/python2", "/usr/bin/python2", 
+			           "./rssgossip.py", phrase, (char*)NULL, vars) == -1) {
+			/* [ADDED] Above changed from book so that script actually works,
+			 * as /usr/bin/python defaults to python 3 now, and causes the 
+			 * script to throw out multiple errors. If it doesn't work, try
+			 * changing it back to /usr/bin/python
+			 */
+				fprintf(stderr, "Can't run script: %s\n", strerror(errno));
+				return 1;
+			}
 		}
 	}
 	return 0;
